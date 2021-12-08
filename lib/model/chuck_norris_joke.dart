f2fa33ab7c28445a7e9c55c49f6c944963f4e6d2
Armor1215 <armor.1215@gmail.com>
2021-12-08 16:12:57 +0200

    Feat: add bloc

--- lib/model/chuck_norris_joke.dart
+++
@@ -1,9 +0,0 @@
-class ChuckNorrisJoke {
-  String imageUrl;
-  String joke;
-
-  ChuckNorrisJoke({
-    this.imageUrl = 'https://i.imgflip.com/5oltgv.jpg',
-    this.joke = 'Any joke',
-  });
-}
---
+++ lib/presentation/main.dart
@@ -0,0 +1,36 @@
+import 'package:chuck_norris_jokes/di.dart';
+import 'package:chuck_norris_jokes/presentation/common/bloc/app_bloc_observer.dart';
+import 'package:chuck_norris_jokes/presentation/main_screen.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+
+
+Future<void> main() async {
+  await BlocOverrides.runZoned(
+        () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      EquatableConfig.stringify = true;
+      await initDi();
+
+      runApp(const MyApp());
+    },
+    blocObserver: AppBlocObserver(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
